ab0db6bb8ea045bcbbad9f7320e9c9ef790f7c1e
JAVVAJI BHUVI <javvajibhuvi01@gmail.com>
2026-04-19 12:30:18 +0530

    Refactor app.py to include sys and os imports

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 import streamlit as st
 from utils.text_extractor import extract_text
 from utils.preprocess import clean_text
